868a54219194b3d23e4b98146b403002db6c9c17
shuriken <shurikenken.info@gmail.com>
2022-06-12 19:10:04 +0900

    feat: featchPopulationsの実装

--- yumemi-project/lib/prefecture.ts
+++ yumemi-project/lib/prefecture.ts
@@ -8,8 +8,32 @@ export interface PrefecturesResult {
   prefName: string
 }
 
+export interface FetchPopulationParams {
+  prefCode: string
+  cityCode: string
+}
+
+export interface Populations {
+  message: null
+  result: {
+    boundaryYear: number
+    data: { label: string; data: { year: number; value: number }[] }[]
+  }
+}
+
 export const fetchPrefectures = async () => {
   const url = "https://opendata.resas-portal.go.jp/api/v1/prefectures"
+  const API_KEY = String(process.env.RESAS_API_KEY)
+  const res = await fetch(url, {
+    headers: { "X-API-KEY": API_KEY },
+  })
+    .then((response) => response.json())
+    .catch(console.error)
+  return res
+}
+
+export const fetchPopulation = async (params: FetchPopulationParams) => {
+  const url = `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=${params.cityCode}&prefCode=${params.prefCode}`
   const API_KEY = String(process.env.RESAS_API_KEY)
   const res = await fetch(url, {
     headers: { "X-API-KEY": API_KEY },
--- yumemi-project/pages/index.tsx
+++ yumemi-project/pages/index.tsx
@@ -1,23 +1,37 @@
-import { Prefectures, fetchPrefectures } from "../lib/prefecture"
+import {
+  Prefectures,
+  fetchPrefectures,
+  fetchPopulation,
+  Populations,
+} from "../lib/prefecture"
 import { InferGetStaticPropsType } from "next"
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>
 
-export default function Home({ prefectures }: Props) {
+export default function Home({ prefectures, populations }: Props) {
   return (
     <div>
-      {prefectures.result.map((result: any) => (
-        <div key={result.prefCode}>{result.prefName}</div>
-      ))}
+      <div>
+        {prefectures.result.map((result: any) => (
+          <div key={result.prefCode}>{result.prefName}</div>
+        ))}
+      </div>
+      <div>{String(populations.result.data[0].data[8].value)}</div>
     </div>
   )
 }
 
 export const getStaticProps = async () => {
   const prefectures: Prefectures = await fetchPrefectures()
+  const populations: Populations = await fetchPopulation({
+    prefCode: "1",
+    cityCode: "-",
+  })
+  console.log(populations)
   return {
     props: {
       prefectures,
+      populations,
     },
   }
 }
